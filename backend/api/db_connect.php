f519a4a549f109ba3734c6b9eb003a7ac9eebabc
sethumathavan ravi <sniffskull@gmail.com>
2025-11-15 14:47:49 +0800

    Refactor API headers: centralize CORS and content type settings in db_connect.php, removing redundant headers from individual API files.

--- backend/api/db_connect.php
+++ backend/api/db_connect.php
@@ -1,5 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
